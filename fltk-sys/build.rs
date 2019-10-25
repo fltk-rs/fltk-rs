a9d34bc8a26e6b88a108cd1c4bca62bb51d69a0f
Mohammed Alyousef <may642_2000@hotmail.com>
2019-10-25 09:52:59 +0300

    Update build.rs

--- fltk-sys/build.rs
+++ fltk-sys/build.rs
@@ -4,41 +4,21 @@ extern crate cmake;
 
 use std::env;
 use std::path::{Path, PathBuf};
-use std::fs;
-
 
 
 fn main() {
 
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
-    let sys_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
+    let manifest_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
     let target_os = env::var("CARGO_CFG_TARGET_OS");
 
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-changed=cfltk/cfl.h");
     println!("cargo:rerun-if-changed=cfltk/cfl_window.h");
-
-    if !cfg!(windows) {
-        let bindings = bindgen::Builder::default()
-        .header("cfltk/cfl.h")
-        // .parse_callbacks(Box::new(bindgen::CargoCallbacks))
-        .generate()
-        .expect("Unable to generate bindings");
-
-        bindings
-            .write_to_file(sys_dir.join("src").join("fl.rs"))
-            .expect("Couldn't write bindings!");
-
-        let bindings = bindgen::Builder::default()
-            .header("cfltk/cfl_window.h")
-            // .parse_callbacks(Box::new(bindgen::CargoCallbacks))
-            .generate()
-            .expect("Unable to generate bindings");
-
-        bindings
-            .write_to_file(sys_dir.join("src").join("window.rs"))
-            .expect("Couldn't write bindings!");
-    }
+    
+    Command::new("git").args(&["submodule", "update", "--init"])
+                      .current_dir(manifest_dir)
+                      .status().unwrap();
 
     let dst = cmake::Config::new("cfltk")
                  .generator("Ninja")
@@ -47,25 +27,10 @@ fn main() {
     println!("cargo:rustc-link-search=native={}", dst.join("build").display());
     println!("cargo:rustc-link-search=native={}", dst.join("lib").display());
     println!("cargo:rustc-link-lib=static=cfltk");
+    println!("cargo:rustc-link-lib=static=fltkd");
+    println!("cargo:rustc-link-lib=static=fltk");
     
-    if cfg!(debug_assertions) && cfg!(windows) {
-        println!("cargo:rustc-link-lib=static=fltkd");
-    } else {
-        println!("cargo:rustc-link-lib=static=fltk");
-    }
-    
-
-
     match target_os.unwrap().as_str() {
-        "linux" => {
-            println!("cargo:rustc-link-lib=dylib=stdc++");
-            println!("cargo:rustc-link-lib=dylib=X11");
-            println!("cargo:rustc-link-lib=dylib=Xext");
-            println!("cargo:rustc-link-lib=dylib=Xinerama");
-            println!("cargo:rustc-link-lib=dylib=Xcursor");
-            println!("cargo:rustc-link-lib=dylib=Xrender");
-            println!("cargo:rustc-link-lib=dylib=Xfixes");
-        },
         "macos" => {
             println!("cargo:rustc-link-lib=dylib=c++");
             println!("cargo:rustc-link-lib=framework=Carbon");
@@ -86,6 +51,14 @@ fn main() {
             println!("cargo:rustc-link-lib=dylib=user32");
             println!("cargo:rustc-link-lib=dylib=kernel32");
         },
-        _ => panic!("OS not supported!")
+        _ => {
+            println!("cargo:rustc-link-lib=dylib=stdc++");
+            println!("cargo:rustc-link-lib=dylib=X11");
+            println!("cargo:rustc-link-lib=dylib=Xext");
+            println!("cargo:rustc-link-lib=dylib=Xinerama");
+            println!("cargo:rustc-link-lib=dylib=Xcursor");
+            println!("cargo:rustc-link-lib=dylib=Xrender");
+            println!("cargo:rustc-link-lib=dylib=Xfixes");
+        }
     }
 }
