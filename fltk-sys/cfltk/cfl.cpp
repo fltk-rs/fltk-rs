e72273b808746ee759648373b21b9c9018f4da30
MoAlyousef <may642_2000@hotmail.com>
2020-04-16 04:52:18 +0300

    change new to malloc

--- fltk-sys/cfltk/cfl.cpp
+++ fltk-sys/cfltk/cfl.cpp
@@ -20,7 +20,7 @@ int Fl_event(void) { return Fl::event(); }
 int Fl_event_key(void) { return Fl::event_key(); }
 
 const char *Fl_event_text(void) {
-  char *buf = new (std::nothrow) char[Fl::event_length() + 1];
+  char *buf = (char*)malloc(Fl::event_length() + 1);
   strncpy(buf, Fl::event_text(), Fl::event_length() + 1);
   return buf;
 }
