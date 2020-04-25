d73b0df92671efda25171a24a680f860b2e61297
MoAlyousef <may642_2000@hotmail.com>
2020-04-25 21:07:01 +0300

    typo

--- src/lib.rs
+++ src/lib.rs
@@ -110,7 +110,7 @@
 //! }));
 //! ```
 //! 
-//! Handled or ignored events shoult return true, unhandled events should return false.
+//! Handled or ignored events should return true, unhandled events should return false.
 //!
 //! # Theming
 //! FLTK offers 4 application themes (called schemes):
