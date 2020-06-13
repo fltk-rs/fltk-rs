05c2729aa23e90608a3ca74d0bd20633d13daad6
MoAlyousef <may642_2000@hotmail.com>
2020-06-13 21:23:35 +0300

    organize data into struct

--- examples/pong.rs
+++ examples/pong.rs
@@ -4,14 +4,24 @@ use std::rc::Rc;
 use std::thread;
 use std::time::Duration;
 
+struct Ball {
+    wid: valuator::FillDial,
+    pos: (i32, i32),       // x and y positions
+    direction: (i32, i32), // x and y directions
+}
+
 fn main() {
     let app = app::App::default();
     let mut wind = window::Window::default()
         .with_size(800, 600)
         .center_screen()
         .with_label("Pong!");
-    let mut ball = valuator::FillDial::new(0, 0, 40, 40, "");
-    ball.set_color(Color::White);
+    let mut ball = Ball {
+        wid: valuator::FillDial::new(0, 0, 40, 40, ""),
+        pos: (0, 0),
+        direction: (1, 1),
+    };
+    ball.wid.set_color(Color::White);
     wind.set_color(Color::Black);
     wind.end();
     wind.show();
@@ -23,10 +33,7 @@ fn main() {
         draw::draw_rectf(*paddle_c.borrow(), 540, 160, 20);
     }));
 
-    let ball_x = &mut 0;
-    let ball_y = &mut 0;
     let paddle_c = paddle_pos.clone();
-
     wind.handle(Box::new(move |ev| {
         match ev {
             enums::Event::Move => {
@@ -36,41 +43,31 @@ fn main() {
             _ => false,
         }
     }));
-    
-    let x_positive = &mut true;
-    let y_positive = &mut true;
+
     while app.wait().unwrap() {
-        if *x_positive {
-            *ball_x += 10;
-        } else {
-            *ball_x -= 10;
-        }
-        if *y_positive {
-            *ball_y += 10;
-        } else {
-            *ball_y -= 10;
-        }
-        if *ball_y == 540 - 40
-            && (*ball_x > *paddle_c.borrow() - 40 && *ball_x < *paddle_c.borrow() + 160)
+        ball.pos.0 += 10 * ball.direction.0;
+        ball.pos.1 += 10 * ball.direction.1;
+        if ball.pos.1 == 540 - 40
+            && (ball.pos.0 > *paddle_c.borrow() - 40 && ball.pos.0 < *paddle_c.borrow() + 160)
         {
-            *y_positive = false;
+            ball.direction.1 = -1;
         }
-        if *ball_y == 0 {
-            *y_positive = true;
+        if ball.pos.1 == 0 {
+            ball.direction.1 = 1;
         }
-        if *ball_x == 800 - 40 {
-            *x_positive = false;
+        if ball.pos.0 == 800 - 40 {
+            ball.direction.0 = -1;
         }
-        if *ball_x == 0 {
-            *x_positive = true;
+        if ball.pos.0 == 0 {
+            ball.direction.0 = 1;
         }
-        if *ball_y > 600 {
-            *ball_x = 0;
-            *ball_y = 0;
-            *x_positive = true;
-            *y_positive = true;
+        if ball.pos.1 > 600 {
+            ball.pos.0 = 0;
+            ball.pos.1 = 0;
+            ball.direction.0 = 1;
+            ball.direction.1 = 1;
         }
-        ball.resize(*ball_x, *ball_y, 40, 40);
+        ball.wid.resize(ball.pos.0, ball.pos.1, 40, 40);
         wind.redraw();
         thread::sleep(Duration::from_millis(16));
     }
