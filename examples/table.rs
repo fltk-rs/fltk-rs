0d45a00531c015a87db579e4374785729a70d96d
MoAlyousef <may642_2000@hotmail.com>
2020-06-17 16:07:39 +0300

    refactor

--- examples/table.rs
+++ examples/table.rs
@@ -22,17 +22,9 @@ fn main() {
 
     table.draw_cell(Box::new(move |ctx, row, col, x, y, w, h| match ctx {
         table::TableContext::StartPage => draw::set_font(Font::Helvetica, 14),
-        table::TableContext::ColHeader => {
-            draw_header(&format!("{}", (col + 65) as u8 as char), x, y, w, h)
-        }
+        table::TableContext::ColHeader => draw_header(&format!("{}", (col + 65) as u8 as char), x, y, w, h),
         table::TableContext::RowHeader => draw_header(&format!("{}", row + 1), x, y, w, h),
-        table::TableContext::Cell => {
-            if table_c.is_selected(row, col) {
-                draw_selected(&format!("{}", row + col), x, y, w, h);
-            } else {
-                draw_data(&format!("{}", row + col), x, y, w, h);
-            }
-        }
+        table::TableContext::Cell => draw_data(&format!("{}", row + col), x, y, w, h, table_c.is_selected(row, col)),
         _ => (),
     }));
 
@@ -47,19 +39,13 @@ fn draw_header(s: &str, x: i32, y: i32, w: i32, h: i32) {
     draw::pop_clip();
 }
 
-fn draw_data(s: &str, x: i32, y: i32, w: i32, h: i32) {
+fn draw_data(s: &str, x: i32, y: i32, w: i32, h: i32, selected: bool) {
     draw::push_clip(x, y, w, h);
-    draw::set_draw_color(Color::White);
-    draw::draw_rectf(x, y, w, h);
-    draw::set_draw_color(Color::Gray0);
-    draw::draw_text2(s, x, y, w, h, Align::Center);
-    draw::draw_rect(x, y, w, h);
-    draw::pop_clip();
-}
-
-fn draw_selected(s: &str, x: i32, y: i32, w: i32, h: i32) {
-    draw::push_clip(x, y, w, h);
-    draw::set_draw_color(Color::from_u32(0xD3D3D3));
+    if selected {
+        draw::set_draw_color(Color::from_u32(0xD3D3D3));
+    } else {
+        draw::set_draw_color(Color::White);
+    }
     draw::draw_rectf(x, y, w, h);
     draw::set_draw_color(Color::Gray0);
     draw::draw_text2(s, x, y, w, h, Align::Center);
