06048e19b5f3b0ef2dd33e978f9edd55f9c313db
Mohammed Alyousef <may642_2000@hotmail.com>
2020-09-04 18:38:09 +0300

    modify load_fonts example

--- fltk/examples/load_fonts.rs
+++ fltk/examples/load_fonts.rs
@@ -4,7 +4,7 @@ fn main() {
     let app = app::App::default();
     let font = app.load_font(&std::path::Path::new("font.ttf")).unwrap();
     let mut wind = window::Window::new(300, 200, 400, 300, "Fonts");
-    let mut frame = frame::Frame::new(0, 0, 400, 100, "Hello");
+    let mut frame = frame::Frame::new(0, 0, 400, 100, &font);
     frame.set_label_size(30);
     frame.set_label_font(Font::by_name(&font));
     wind.set_color(Color::White);
