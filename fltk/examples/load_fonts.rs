4ebfa671192f54060b780348b163b9568349f13e
MoAlyousef <may642_2000@hotmail.com>
2020-09-04 05:03:20 +0300

    example

--- fltk/examples/load_fonts.rs
+++ fltk/examples/load_fonts.rs
@@ -2,11 +2,11 @@ use fltk::*;
 
 fn main() {
     let app = app::App::default();
-    app::load_font("/opt/X11/share/fonts/TTF/luximb.ttf", "LuxiMono-Bold").unwrap();
+    app::load_font("font.ttf", "Comic Sans MS").unwrap();
     let mut wind = window::Window::new(300, 200, 400, 300, "Fonts");
     let mut frame = frame::Frame::new(0, 0, 400, 100, "Hello");
     frame.set_label_size(30);
-    frame.set_label_font(Font::by_name("LuxiMono-Bold"));
+    frame.set_label_font(Font::by_name("Comic Sans MS"));
     wind.set_color(Color::White);
     wind.end();
     wind.show();
