f309f3e878c495d61ef86fd3760d6b2a5a54f110
MoAlyousef <may642_2000@hotmail.com>
2020-10-29 19:14:43 +0300

    update docs on dependencies

--- fltk/src/lib.rs
+++ fltk/src/lib.rs
@@ -161,6 +161,28 @@
 //! - fltk-bundled: Support for bundled versions of cfltk and fltk on selected platforms (requires curl and tar)
 //! - enable-glwindow: Support for drawing using OpenGL functions.
 //!
+//! ## Dependencies
+//! 
+//! Rust (version > 1.38), CMake (version > 3.0), Git and a C++11 compiler need to be installed and in your PATH for a crossplatform build from source. This crate also offers a bundled form of fltk on selected platforms, this can be enabled using the fltk-bundled feature flag (which requires curl and tar to download and unpack the bundled libraries).
+//! 
+//! - Windows: No dependencies.
+//! - MacOS: No dependencies.
+//! - Linux: X11 and OpenGL development headers need to be installed for development. 
+//! 
+//! For Debian-based GUI distributions, that means running:
+//! ```
+//! $ sudo apt-get install libx11-dev libxext-dev libxft-dev libxinerama-dev libxcursor-dev libxrender-dev libxfixes-dev libgl1-mesa-dev libglu1-mesa-dev
+//! ```
+//! For RHEL-based GUI distributions, that means running:
+//! ```
+//! $ sudo yum groupinstall "X Software Development" 
+//! ```
+//! For Arch-based GUI distributions, that means running:
+//! ```
+//! $ sudo pacman -S libx11 libxext libxft libxinerama libxcursor libxrender libxfixes libgl mesa --needed
+//! ```
+//! If you have ninja-build installed, you can enable it using the "use-ninja" feature. This should accelerate build times significantly.
+//!
 //! ## FAQ
 //!
 //! please check the [FAQ](https://github.com/MoAlyousef/fltk-rs/blob/master/FAQ.md) page for frequently asked questions, encountered issues, guides on deployment, and contribution.
