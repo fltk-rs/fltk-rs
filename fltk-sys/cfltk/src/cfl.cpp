b70d3acda67e47f6158414991ec8471dd88c7775
MoAlyousef <may642_2000@hotmail.com>
2020-11-02 15:07:45 +0300

    fix for msvc

--- fltk-sys/cfltk/src/cfl.cpp
+++ fltk-sys/cfltk/src/cfl.cpp
@@ -23,9 +23,8 @@
 #define USE_XFT 1
 #endif
 
-// from stackoverflow answer: https://stackoverflow.com/a/33389908/9698906
 #if defined(_MSC_VER) && _MSC_VER < 1900
-#define snprintf(buf,len, format,...) _snprintf_s(buf, len,len, format, __VA_ARGS__)
+#define snprintf _snprintf
 #endif
 
 int Fl_run(void) {
