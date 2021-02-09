ca973ecc2b97232dda5d7707c88dba9e53597324
MoAlyousef <may642_2000@hotmail.com>
2021-02-09 20:32:16 +0300

    0.14.5

--- fltk/examples/popup.rs
+++ fltk/examples/popup.rs
@@ -3,13 +3,16 @@ use fltk::*;
 fn main() {
     let app = app::App::default();
     let mut wind = window::Window::default().with_size(400, 300);
-    let mut frame = frame::Frame::default().size_of(&wind);
+    let mut frame = frame::Frame::default()
+        .size_of(&wind)
+        .with_label("Right click me!");
     wind.end();
     wind.show();
     let mut menu = menu::MenuItem::new(&["1st menu item\t", "2nd menu item\t", "3rd menu item\t"]);
     frame.handle(move |ev| match ev {
         Event::Push => {
-            if app::event_button() == 3 { // or app::event_mouse_button() == Mouse::Right
+            if app::event_mouse_button() == Mouse::Right {
+                // or app::event_button() == 3
                 let coords = app::event_coords();
                 match menu.popup(coords.0, coords.1) {
                     None => println!("No value was chosen!"),
