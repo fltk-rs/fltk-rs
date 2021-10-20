737c7971203cffd86bcba665c9bd1bcc0ea35fde
MoAlyousef <may642_2000@hotmail.com>
2021-10-20 15:14:20 +0300

    fix terminal backspace for non-ascii chars

--- fltk/examples/terminal.rs
+++ fltk/examples/terminal.rs
@@ -139,10 +139,11 @@ impl Term {
                     }
                     Key::BackSpace => {
                         if !cmd.is_empty() {
+                            let c = cmd.pop().unwrap();
+                            let len = if c.is_ascii() { 1 } else { 2 };
                             let text_len = t.text().len() as i32;
-                            t.buffer().unwrap().remove(text_len - 1, text_len);
-                            sbuf.remove(text_len - 1, text_len);
-                            cmd.pop().unwrap();
+                            t.buffer().unwrap().remove(text_len - len, text_len);
+                            sbuf.remove(text_len - len, text_len);
                             true
                         } else {
                             false
