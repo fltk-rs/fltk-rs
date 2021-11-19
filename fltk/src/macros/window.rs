49f53211a1a457da4b56c3a9a7d6d39ab220d93a
MoAlyousef <may642_2000@hotmail.com>
2021-11-19 18:36:58 +0300

    update raw-window-handle

--- fltk/src/macros/window.rs
+++ fltk/src/macros/window.rs
@@ -7,11 +7,10 @@ macro_rules! impl_window_ext {
             fn raw_window_handle(&self) -> RawWindowHandle {
                 #[cfg(target_os = "windows")]
                 {
-                    return RawWindowHandle::Windows(windows::WindowsHandle {
-                        hwnd: self.raw_handle(),
-                        hinstance: crate::app::display(),
-                        ..windows::WindowsHandle::empty()
-                    });
+                    let mut handle = Win32Handle::empty();
+                    handle.hwnd = self.raw_handle();
+                    handle.hinstance = crate::app::display();
+                    return RawWindowHandle::Win32(handle);
                 }
 
                 #[cfg(target_os = "macos")]
@@ -21,19 +20,17 @@ macro_rules! impl_window_ext {
                         pub fn my_getContentView(xid: *mut raw::c_void) -> *mut raw::c_void;
                     }
                     let cv = unsafe { my_getContentView(raw) };
-                    return RawWindowHandle::MacOS(macos::MacOSHandle {
-                        ns_window: raw,
-                        ns_view: cv as _,
-                        ..macos::MacOSHandle::empty()
-                    });
+                    let mut handle = AppKitHandle::empty();
+                    handle.ns_window = raw;
+                    handle.ns_view = cv as _;
+                    return RawWindowHandle::AppKit(handle);
                 }
 
                 #[cfg(target_os = "android")]
                 {
-                    return RawWindowHandle::Android(android::AndroidHandle {
-                        a_native_window: self.raw_handle(),
-                        ..android::AndroidHandle::empty()
-                    });
+                    let mut handle = AndroidNdkHandle::empty();
+                    handle.a_native_window = self.raw_handle();
+                    return RawWindowHandle::AndroidNdk(handle);
                 }
 
                 #[cfg(any(
@@ -44,11 +41,10 @@ macro_rules! impl_window_ext {
                     target_os = "openbsd",
                 ))]
                 {
-                    return RawWindowHandle::Xlib(unix::XlibHandle {
-                        window: self.raw_handle(),
-                        display: crate::app::display(),
-                        ..unix::XlibHandle::empty()
-                    });
+                    let mut handle = XlibHandle::empty();
+                    handle.window = self.raw_handle();
+                    handle.display = crate::app::display();
+                    return RawWindowHandle::Xlib(handle);
                 }
             }
         }
