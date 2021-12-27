793a162ee045eb518ca42158c18c6d95c7850dd7
MoAlyousef <may642_2000@hotmail.com>
2021-12-28 01:35:51 +0300

    remove is_derived checks for some table methods

--- fltk/src/macros/table.rs
+++ fltk/src/macros/table.rs
@@ -538,7 +538,7 @@ macro_rules! impl_table_ext {
 
                 fn scrollbar(&self) -> $crate::valuator::Scrollbar {
                     assert!(!self.was_deleted());
-                    assert!(self.is_derived);
+                    // assert!(self.is_derived);
                     unsafe {
                         let ptr = [<$flname _scrollbar>](self.inner);
                         assert!(!ptr.is_null());
@@ -550,7 +550,7 @@ macro_rules! impl_table_ext {
 
                 fn hscrollbar(&self) -> $crate::valuator::Scrollbar {
                     assert!(!self.was_deleted());
-                    assert!(self.is_derived);
+                    // assert!(self.is_derived);
                     unsafe {
                         let ptr = [<$flname _hscrollbar>](self.inner);
                         assert!(!ptr.is_null());
@@ -567,7 +567,7 @@ macro_rules! impl_table_ext {
                     col: i32,
                 ) -> Option<(i32, i32, i32, i32)> {
                     assert!(!self.was_deleted());
-                    assert!(self.is_derived);
+                    // assert!(self.is_derived);
                     let mut x = 0;
                     let mut y = 0;
                     let mut w = 0;
