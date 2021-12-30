a0888568c6f4dd17e7cb7c6baf06d1f0d16777f5
MoAlyousef <may642_2000@hotmail.com>
2021-12-30 12:51:17 +0300

    update docs regarding ninja

--- fltk/src/lib.rs
+++ fltk/src/lib.rs
@@ -253,7 +253,7 @@ $ nix-shell --packages rustc cmake git gcc xorg.libXext xorg.libXft xorg.libXine
 ## Features
 
 The following are the features offered by the crate:
-- use-ninja: Uses the ninja build system for a faster build, especially on Windows.
+- use-ninja: Uses the ninja build system if available for a faster build, especially on Windows.
 - fltk-bundled: Support for bundled versions of cfltk and fltk on selected platforms (requires curl and tar)
 - no-pango: Build without pango support on Linux/BSD.
 - enable-glwindow: Support for drawing using OpenGL functions.
