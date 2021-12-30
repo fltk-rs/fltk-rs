be3daa679a19776a123027a0ebb025929cacd24a
MoAlyousef <may642_2000@hotmail.com>
2021-12-30 11:48:36 +0300

    remove doc include_str

--- fltk/src/examples/mod.rs
+++ fltk/src/examples/mod.rs
@@ -1,6 +1,30 @@
-//! Documentation for the examples
-//!
-#![doc = include_str!("../../examples/README.md")]
+/*!
+Documentation for the examples
+
+Each file is a standalone executable with its own main function. Examples vary from basic hello world simplicity to some more complex examples. They may contain logic bugs, however their purpose is for demonstration and for testing UI elements. If you would like to contribute fixes, improvements or more examples, you're very welcome to do so.
+
+To run the examples:
+```
+$ cargo run --example editor
+$ cargo run --example calculator
+$ cargo run --example calculator2
+$ cargo run --example tabs
+$ cargo run --example terminal
+$ cargo run --example counter
+$ cargo run --example hello
+$ cargo run --example hello_button
+$ cargo run --example paint
+$ cargo run --example pong
+$ cargo run --example fb
+$ cargo run --example custom_widgets
+$ cargo run --example <filename>
+```
+
+More interesting examples can be found in the fltk-rs-demos [repo](https://github.com/fltk-rs/demos).
+Also a nice implementation of the 7guis tasks can be found [here](https://github.com/tdryer/7guis-fltk-rs).
+Various advanced examples can also be found [here](https://github.com/wyhinton/FLTK-RS-Examples).
+*/
+
 pub mod animations;
 pub mod calculator;
 pub mod calculator2;
