c3bb49bf8db46cd070c5e1484f0ffb3fe4241434
MoAlyousef <may642_2000@hotmail.com>
2022-03-24 00:47:39 +0300

    update grid example

--- fltk/examples/grid.rs
+++ fltk/examples/grid.rs
@@ -6,14 +6,26 @@ struct Grid {
     table: table::Table,
 }
 
+impl Default for Grid {
+    fn default() -> Self {
+        Grid::new(0, 0, 0, 0, None)
+    }
+}
+
 impl Grid {
-    pub fn new() -> Self {
-        let mut table = table::Table::default().size_of_parent();
+    pub fn new<S: Into<Option<&'static str>>>(x: i32, y: i32, w: i32, h: i32, label: S) -> Self {
+        let mut table = table::Table::new(x, y, w, h, label);
         table.set_frame(enums::FrameType::NoBox);
         table.set_scrollbar_size(-1);
         table.end();
         Self { table }
     }
+
+    pub fn default_fill() -> Self {
+        let g = Grid::default();
+        Self { table: g.table.size_of_parent().center_of_parent() }
+    }
+
     pub fn set_layout(&mut self, rows: i32, cols: i32) {
         self.table.set_rows(rows);
         self.table.set_cols(cols);
@@ -31,10 +43,11 @@ impl Grid {
 
     pub fn debug(&mut self, flag: bool) {
         if flag {
-            self.table.draw_cell(move |_, ctx, _, _, x, y, w, h| match ctx {
+            self.table.draw_cell(move |_, ctx, row, col, x, y, w, h| match ctx {
                 table::TableContext::Cell => {
-                    draw::set_draw_color(enums::Color::Black);
+                    draw::set_draw_color(enums::Color::Red);
                     draw::draw_rect(x, y, w, h);
+                    draw::draw_text2(&format!("{},{}", row, col), x, y, w, h, enums::Align::Center);
                 },
                 _ => (),
             });
@@ -45,11 +58,11 @@ impl Grid {
 fn main() {
     let a = app::App::default();
     let mut win = window::Window::default().with_size(500, 400);
-    let mut grid = Grid::new();
+    let mut grid = Grid::default_fill();
     grid.set_layout(10, 5);
-    // grid.debug(true);
+    grid.debug(false); // set to true to see cell outlines
     win.end();
-    
+
     grid.add_widget(&mut frame::Frame::default().with_label("Employee Form"), 0, 1, 3, 1);
     grid.add_widget(&mut frame::Frame::default().with_label("Name"), 2, 1, 1, 1);
     let mut name = input::Input::default();
@@ -62,8 +75,8 @@ fn main() {
     grid.add_widget(&mut occupation, 6, 3, 1, 1);
     let mut btn = button::Button::default().with_label("Submit");
     grid.add_widget(&mut btn, 8, 2, 1, 1);
+    
     win.show();
-
 
     btn.set_callback(move |_| {
         println!("Name: {}", name.value());
