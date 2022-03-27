83853cf8ec555a7b78976c98040dd1c719cd9615
MoAlyousef <may642_2000@hotmail.com>
2022-03-27 16:31:18 +0300

    update examples

---
+++ fltk/src/examples/screens_info.rs
@@ -0,0 +1,3 @@
+//!Documentation for the `screens_info` example
+//!
+//!This is the documentation for the example `screens_info.rs`. The code can be found in the `examples` directory next to the `src` directory in the source distribution.
