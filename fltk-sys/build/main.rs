60f1aaef4294b6619bd5dcb38059f3d3f8c72a5c
MoAlyousef <may642_2000@hotmail.com>
2022-08-09 03:31:03 +0300

    enable more bundles for upcoming arm64 macos bundle

--- fltk-sys/build/main.rs
+++ fltk-sys/build/main.rs
@@ -20,11 +20,11 @@ fn main() {
     println!("cargo:rerun-if-changed=build/utils.rs");
 
     if cfg!(feature = "fltk-bundled") {
-        bundled::get(&target_os, &out_dir);
+        bundled::get(&target_os, &target_triple, &out_dir);
     } else if !crate::utils::has_program("git") || !crate::utils::has_program("cmake") {
         println!("cargo:warning=Could not find invokable CMake or Git, building using the fltk-bundled feature flag!");
         println!("cargo:warning=If this is not desirable, please ensure CMake and Git are installed and in your PATH!");
-        bundled::get(&target_os, &out_dir);
+        bundled::get(&target_os, &target_triple, &out_dir);
     } else {
         source::build(&manifest_dir, &target_triple, &out_dir);
     }
