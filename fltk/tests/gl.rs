95f6533aa2f44ae4a1a6d8e64b1d08d76b8917a1
MoAlyousef <may642_2000@hotmail.com>
2022-09-28 01:43:40 +0300

    update test

--- fltk/tests/gl.rs
+++ fltk/tests/gl.rs
@@ -1,18 +1,19 @@
-// use fltk::{prelude::*, image::*, enums::*, *};
+// use fltk::{enums::*, prelude::*, *};
 
-// extern "C" {
-//     fn glUseProgram(v: u32);
-// }
-
-fn main () {
-    // let a = app::App::default();
-    // unsafe { glUseProgram(0); }
-    // let mut win = window::GlWindow::default().with_size(400, 300).with_label("mainwin");
+fn main() {
+    // let app = app::App::default();
+    // let mut win = window::GlWindow::default().with_size(400, 300);
+    // win.set_mode(Mode::Opengl3 | Mode::MultiSample); // for antialiasing
     // win.set_frame(FrameType::FlatBox);
     // win.set_color(Color::White);
-    // let mut b = button::Button::new(100, 100, 80, 39, "Click");
+    // win.begin();
+    // let mut dial =
+    //     valuator::LineDial::new(100, 50, 200, 200, "Load %");
+    // dial.set_selection_color(Color::Red);
+    // dial.set_color(Color::Blue);
+    // dial.set_value(0.5);
+    // dial.set_frame(FrameType::OFlatFrame);
     // win.end();
     // win.show();
-
-    // a.run().unwrap();
+    // app.run().unwrap();
 }
