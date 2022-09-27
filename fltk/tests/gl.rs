a3eee14fc92f527e73465dc6c7691ed676ebcce0
MoAlyousef <may642_2000@hotmail.com>
2022-09-28 01:12:12 +0300

    add test

---
+++ fltk/tests/gl.rs
@@ -0,0 +1,18 @@
+// use fltk::{prelude::*, image::*, enums::*, *};
+
+// extern "C" {
+//     fn glUseProgram(v: u32);
+// }
+
+fn main () {
+    // let a = app::App::default();
+    // unsafe { glUseProgram(0); }
+    // let mut win = window::GlWindow::default().with_size(400, 300).with_label("mainwin");
+    // win.set_frame(FrameType::FlatBox);
+    // win.set_color(Color::White);
+    // let mut b = button::Button::new(100, 100, 80, 39, "Click");
+    // win.end();
+    // win.show();
+
+    // a.run().unwrap();
+}
