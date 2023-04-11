72548092fc8b8d86ee0657d29a602fe7d30b0290
MoAlyousef <may642_2000@hotmail.com>
2023-04-12 02:22:53 +0300

    update example name

--- fltk/src/examples/shapedwindow.rs
+++
@@ -1,3 +0,0 @@
-//!Documentation for the `shapedwindow` example
-//!
-//!This is the documentation for the example `shapedwindow.rs`. The code can be found in the `examples` directory next to the `src` directory in the source distribution.
---
+++ fltk/src/examples/shapedwindow_taskbar.rs
@@ -0,0 +1,3 @@
+//!Documentation for the `shapedwindow_taskbar` example
+//!
+//!This is the documentation for the example `shapedwindow_taskbar.rs`. The code can be found in the `examples` directory next to the `src` directory in the source distribution.
