7060794dc31cd7abfc4d0765fc90e937b62a7009
MoAlyousef <may642_2000@hotmail.com>
2023-07-09 15:34:38 +0300

    add docs

--- fltk/src/app/opts.rs
+++ fltk/src/app/opts.rs
@@ -1,5 +1,6 @@
-use fltk_sys::fl::*;
+use fltk_sys::fl;
 
+/// global FLTK options
 #[repr(i32)]
 #[non_exhaustive]
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
@@ -10,43 +11,45 @@ pub enum Option {
     ///
     /// When switched off (default), the cursor will stop at the end of the text.
     /// Pressing Tab or Ctrl-Tab will advance the keyboard focus.
-    arrowFocus=0,
+    FrrowFocus=0,
     /// If visible focus is switched on (default), FLTK will draw a dotted rectangle
     /// inside the widget that will receive the next keystroke. If switched
     /// off, no such indicator will be drawn and keyboard navigation
     /// is disabled.
-    visibleFocus,
+    VisibleFocus,
       /// If text drag-and-drop is enabled (default), the user can select and drag text
       /// from any text widget. If disabled, no dragging is possible, however
       /// dropping text from other applications still works.
-    dndText,
+    DndText,
       /// If tooltips are enabled (default), hovering the mouse over a widget with a
       /// tooltip text will open a little tooltip window until the mouse leaves
       /// the widget. If disabled, no tooltip is shown.
-    showTooltips,
+    ShowTooltips,
      /// When switched on (default), Fl_Native_File_Chooser runs GTK file dialogs
      /// if the GTK library is available on the platform (linux/unix only).
      /// When switched off, GTK file dialogs aren't used even if the GTK library is available.
-    fnfcUsesGtk,
+    FnfcUsesGtk,
     /// When switched on (default), Fl_Printer runs the GTK printer dialog
     /// if the GTK library is available on the platform (linux/unix only).
     /// When switched off, the GTK printer dialog isn't used even if the GTK library is available.
-    printerUsesGtk,
+    PrinterUsesGtk,
     /// When switched on (default), the library shows in a transient yellow window the zoom factor
     /// value.
     /// When switched off, no such window gets displayed.
-    showScaling,
+    ShowScaling,
     /// Meaningful for the Wayland/X11 platform only. When switched on (default), the library uses a Zenity-based file dialog.
     /// When switched off, the GTK file dialog is used instead.
-    fnfcUsesZenity,
+    FnfcUsesZenity,
 }
 
+/// Get the option's value
 pub fn option(opt: Option) -> bool {
     unsafe {
         fl::Fl_option(opt as i32) != 0
     }
 }
 
+/// Set the option's value
 pub fn set_option(opt: Option, val: bool) {
     unsafe {
         fl::Fl_set_option(opt as i32, val as i32)
