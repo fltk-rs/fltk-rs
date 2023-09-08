8ceb57f23ee9e90ab79c2f36013caf43e498a5da
VirusCamp <viruscamp@gmail.com>
2023-09-08 13:51:52 +0800

    enable feature `doc_auto_cfg` for docs.rs

--- fltk-sys/src/lib.rs
+++ fltk-sys/src/lib.rs
@@ -1,3 +1,5 @@
+#![cfg_attr(docsrs, feature(doc_auto_cfg))]
+#![cfg_attr(docsrs, allow(unused_attributes))]
 #![doc = include_str!("../README.md")]
 #![allow(non_camel_case_types)]
 #![allow(dead_code)]
