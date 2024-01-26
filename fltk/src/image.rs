6633faae385812971d460dfc195b4c44b9d2c211
Mohammed Alyousef <may642_2000@hotmail.com>
2024-01-26 16:07:22 +0300

    local fix for AnimGif::from_data

--- fltk/src/image.rs
+++ fltk/src/image.rs
@@ -465,6 +465,8 @@ pub struct AnimGifImage {
 
 crate::macros::image::impl_image_ext!(AnimGifImage, Fl_Anim_GIF_Image);
 
+static AnimGifCount: std::sync::atomic::AtomicU8 = std::sync::atomic::AtomicU8::new(0);
+
 impl AnimGifImage {
     /// Loads the image from a filesystem path, doesn't check for the validity of the data
     /// # Errors
@@ -517,8 +519,13 @@ impl AnimGifImage {
             if data.is_empty() {
                 Err(FltkError::Internal(FltkErrorKind::ResourceNotFound))
             } else {
+                let mut name = AnimGifCount.load(std::sync::atomic::Ordering::Relaxed);
+                let mut name_str = name.to_string();
+                name_str.push('\0');
+                name += 1;
+                AnimGifCount.store(name, std::sync::atomic::Ordering::Relaxed);
                 let x = Fl_Anim_GIF_Image_from(
-                    std::ptr::null(),
+                    name_str.as_ptr() as _,
                     data.as_ptr(),
                     data.len() as _,
                     w.as_widget_ptr() as _,
