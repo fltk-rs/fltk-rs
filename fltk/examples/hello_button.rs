19c872ee2a3195db3d4f45ef0b2ec9980da8dfb8
Mohammed Alyousef <may642_2000@hotmail.com>
2024-01-31 00:37:02 +0300

    revert example and update

--- fltk/examples/hello_button.rs
+++ fltk/examples/hello_button.rs
@@ -1,9 +1,7 @@
-use fltk::{app, button::Button, frame::Frame, prelude::*, window::Window, enums::Color};
+use fltk::{app, button::Button, frame::Frame, prelude::*, window::Window};
 
 fn main() {
     let app = app::App::default();
-    app::set_color(Color::White, 0x00, 0xff, 0xff);
-    assert_eq!(Color::White.to_rgb(), (0x00, 0xff, 0xff));
     let mut wind = Window::default().with_size(400, 300);
     let mut frame = Frame::default().with_size(200, 100).center_of(&wind);
     let mut but = Button::new(160, 210, 80, 40, "Click me!");
