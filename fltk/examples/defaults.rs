5ad833ca09cfeae90874d728c1fff9b8e65facc3
Mohammed Alyousef <may642_2000@hotmail.com>
2024-09-14 14:01:54 +0300

    update defaults example

--- fltk/examples/defaults.rs
+++ fltk/examples/defaults.rs
@@ -1,33 +1,33 @@
-use fltk::{prelude::*, *};
+use fltk::{prelude::*, enums::*, *};
 
 fn main() {
-    let (r, g, b) = utils::hex2rgb(0xfafdf3);
-
     let app = app::App::default();
-
     // global theming
-    app::background(r, g, b); // background color. For input/output and text widgets, use app::background2
-    app::foreground(20, 20, 20); // labels
-    app::set_font(enums::Font::Courier);
+    app::background(55, 55, 55); // background color. 
+    // For input/output and text widgets, use app::background2
+    app::background2(0, 0, 0);
+    app::foreground(255, 255, 255); // labels
     app::set_font_size(16);
-    app::set_frame_type(enums::FrameType::RFlatBox);
+    app::set_frame_type2(FrameType::UpBox, FrameType::RFlatBox);
+    app::set_frame_type2(FrameType::DownBox, FrameType::RFlatBox);
+	app::set_frame_border_radius_max(15); // set the roundness of the RFlatBox
+	app::set_font(Font::Times);
     app::set_visible_focus(false);
 
     // regular widget code
     let mut win = window::Window::default().with_size(400, 300);
-    let frame = frame::Frame::new(0, 0, 400, 200, "Defaults");
-    let flex = group::Flex::default()
-        .with_size(400, 50)
-        .below_of(&frame, 50);
-    let mut but1 = button::Button::default().with_label("Button1");
-    but1.set_color(enums::Color::Yellow);
-    but1.set_down_frame(enums::FrameType::RFlatBox);
-    frame::Frame::default();
-    let mut but2 = button::Button::default().with_label("Button2");
-    but2.set_color(enums::Color::Yellow);
-    but2.set_down_frame(enums::FrameType::RFlatBox);
+    let mut flex = group::Flex::default_fill().column();
+	flex.set_margins(100, 60, 100, 60);
+	flex.set_pad(10);
+    let mut btn1 = button::Button::default().with_label("Increment");
+	btn1.set_color(Color::Red.darker());
+	btn1.set_selection_color(Color::Red.darker().darker());
+	let mut out = frame::Frame::default().with_label("0");
+	out.set_color(Color::Green.darker());
+	let mut btn2 = button::Button::default().with_label("Decrement");
+	btn2.set_color(Color::Red.darker());
+	btn2.set_selection_color(Color::Red.darker().darker());
     flex.end();
-
     win.end();
     win.show();
 
