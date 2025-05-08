bfdb4ab072859c3c5afbcca09c0e3480d91c1897
Mohammed Alyousef <may642_2000@hotmail.com>
2025-05-08 12:08:14 +0300

    disable snake_case warning for fltk-sys

--- fltk-sys/src/lib.rs
+++ fltk-sys/src/lib.rs
@@ -2,6 +2,7 @@
 #![cfg_attr(docsrs, allow(unused_attributes))]
 #![doc = include_str!("../README.md")]
 #![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
 #![allow(dead_code)]
 #![allow(non_upper_case_globals)]
 #![no_std]
