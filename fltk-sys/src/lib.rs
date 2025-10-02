0f717cb3d2371c04e654cdd70467383d41167544
Mohammed Alyousef <may642_2000@hotmail.com>
2025-10-03 02:47:42 +0300

    remove doc_auto_cfg

--- fltk-sys/src/lib.rs
+++ fltk-sys/src/lib.rs
@@ -1,5 +1,3 @@
-#![cfg_attr(docsrs, feature(doc_auto_cfg))]
-#![cfg_attr(docsrs, allow(unused_attributes))]
 #![doc = include_str!("../README.md")]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
